e247f864776ac5652f24b204e01880d158d2a6af
kimjuhe <58322511+kimjuhe@users.noreply.github.com>
2020-10-04 23:49:59 +0900

    Delete TicketingSystem.java

--- TicketingSystem.java
+++
@@ -1,9 +0,0 @@
-package hw;
-
-public class TicketingSystem {
-	public static void main(String[] args) {
-		System.out.println("명품콘서트홀 예약 시스템입니다.");
-		Reservation r = new Reservation();
-		r.Run();
-	}
-}
